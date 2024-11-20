e67bb25f22e0ba54ccbd1b3a2e2f922ebad3d427
bpkorea <calvin.heo@blueprism.kr>
2024-11-21 08:22:38 +0900

    Updated Lambda function message 2

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 def lambda_handler(event, context):
     return {
         "statusCode": 200,
-        "body": "Updated message: Hello, AWS CodePipeline!"
+        "body": "Updated message: Hello 2, AWS CodePipeline!"
     }
 
